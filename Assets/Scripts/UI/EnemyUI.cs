09cd5527618185ce82160e9d8539651a293a31cf
MartinB04 <martin.bibiano4@outlook.com>
2024-06-28 09:22:40 -0600

    Change: Ocultamiento de enemyLifeBar agregado.
    *Se redimencionó la barra de vida.
    *Si la enemyLife esta al maximo, el slider se oculta.
    *El slider aparece si el enemigo sufre daño.

--- Assets/Scripts/UI/EnemyUI.cs
+++ Assets/Scripts/UI/EnemyUI.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
 public class EnemyUI : MonoBehaviour
 {
     [SerializeField] Transform enemyTarget;
-    //[SerializeField] TextMeshProUGUI enemyLife;
-    [SerializeField] Slider enemyLife;
+    
+    [SerializeField] Slider sliderEnemyLife;
     private Canvas canvasEnemyUI;
     private Transform enemyTransform;
 
+    [SerializeField] int enemyMaxLife = 100;
+
+    private Image enemyLifeFillImage;
+
+    private Image enemyLifebackgroundImage;
 
     [SerializeField] Vector2 offsetLife;
 
@@ -25,13 +31,16 @@ public class EnemyUI : MonoBehaviour
         this.canvasEnemyUI = GetComponentInChildren<Canvas>();
         this.enemyTransform = GetComponent<Transform>();
         this.mainCamera = Camera.main;
+
+        this.enemyLifeFillImage = this.sliderEnemyLife.fillRect.GetComponent<Image>();
+        
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        if (enemyLife == null)
+        if (this.sliderEnemyLife == null)
             Debug.LogWarning("Nulo");
         if (this.mainCamera == null)
             Debug.LogWarning("MainCamera no asignada.");
@@ -40,32 +49,24 @@ public class EnemyUI : MonoBehaviour
 
         FixLocalScale();
 
-        enemyLife.maxValue = CollisionHandler.sharedInstance.GetEnemyMaxLife();
-        enemyLife.value = CollisionHandler.sharedInstance.GetEnemyLife();
+        sliderEnemyLife.maxValue = CollisionHandler.sharedInstance.GetEnemyMaxLife();
+        sliderEnemyLife.value = CollisionHandler.sharedInstance.GetEnemyLife();
 
-        enemyLife.transform.position = new Vector2(this.enemyTarget.position.x + this.offsetLife.x, this.enemyTarget.position.y + this.offsetLife.y);
+        sliderEnemyLife.transform.position = new Vector2(this.enemyTarget.position.x + this.offsetLife.x, this.enemyTarget.position.y + this.offsetLife.y);
+
+        enemyLifebackgroundImage = GetComponentsInChildren<Image>(true).FirstOrDefault();
+
+        ShowEnemyLifeBar();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log($"{CollisionHandler.sharedInstance.GetEnemyLife()}");
-        Debug.Log($"position fireskull {this.enemyTarget.position}");
-       
-        enemyLife.value = CollisionHandler.sharedInstance.GetEnemyLife();
-
-        enemyLife.transform.position = new Vector2(this.enemyTarget.position.x + this.offsetLife.x, this.enemyTarget.position.y + this.offsetLife.y);
-        //enemyLife.transform.position = this.enemyTarget.position;
-        //enemyLife.transform.localScale = new Vector3(1, 1, 1);
-
-       
-
-
+        sliderEnemyLife.value = CollisionHandler.sharedInstance.GetEnemyLife();
+        sliderEnemyLife.transform.position = new Vector2(this.enemyTarget.position.x + this.offsetLife.x, this.enemyTarget.position.y + this.offsetLife.y);
+        
+        ShowEnemyLifeBar();
         FixLocalScale();
-        
-
-
-        
     }
 
     private void FixLocalScale()
@@ -74,5 +75,20 @@ public class EnemyUI : MonoBehaviour
             this.canvasEnemyUI.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         else if (this.enemyTransform.localScale.x == -1)
             this.canvasEnemyUI.transform.localScale = new Vector3(-0.1f, 0.1f, 0.1f);
+    }
+
+    public void ShowEnemyLifeBar()
+    {
+        //si la vida del enemy esta llena no muestra la barra de vida
+        if (this.sliderEnemyLife.value >= this.enemyMaxLife) 
+        { 
+            this.enemyLifeFillImage.enabled = false;
+            enemyLifebackgroundImage.enabled = false;
+        }
+        else 
+        { 
+            this.enemyLifeFillImage.enabled = true;
+            enemyLifebackgroundImage.enabled = true;
+        }
     }
 }
