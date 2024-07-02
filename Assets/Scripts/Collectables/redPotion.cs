eb297072e0de259236ab073812d3166b07402432
MartinB04 <martin.bibiano4@outlook.com>
2024-07-01 22:50:11 -0600

    Add: Animaciones de redpotion agregadas.
    *Se han agregado animaciones para cuando la pocion se ha usado.
    *Se ha implementado una corrutina para la destruccion de la pocion despues de 3s de haber sido consumida al 100%

--- Assets/Scripts/Collectables/redPotion.cs
+++ Assets/Scripts/Collectables/redPotion.cs
@@ -8,10 +8,21 @@ public class redPotion : MonoBehaviour
     [SerializeField] float recoveryLife;
     private float remainingPoints;
 
+    private Animator animator;
+
+    private void Awake()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        if (!this.animator)
+            Debug.LogWarning("Animator no asignado.");
 
+        this.remainingPoints = this.recoveryLife;
+        this.animator.SetBool("full", true);
     }
 
     // Update is called once per frame
@@ -24,16 +35,53 @@ public class redPotion : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            //Debug.Log("recoverylife " + this.recoveryLife);
-
-            this.remainingPoints = LevelSystem.sharedInstance.IncreaseLife(this.recoveryLife);
-            //Debug.Log("recoverylife2 " + this.recoveryLife);
-
-
-            if (this.remainingPoints <= 0)
+            
+            this.remainingPoints = LevelSystem.sharedInstance.IncreaseLife(this.remainingPoints);
+            /*if (this.remainingPoints <= 0)
                 Destroy(gameObject);
             else
                 this.recoveryLife = this.remainingPoints;
+            */
+            Potion();
+
+            if (this.remainingPoints <= 0)
+                StartCoroutine(DestroyPotion());
         }
+    }
+
+
+    private void Potion()
+    {
+
+        if(this.remainingPoints >= this.recoveryLife * 0.5f) 
+        {
+            this.animator.SetBool("full", false);
+            this.animator.SetBool("medium", true);
+            this.animator.SetBool("almostEmpty", false);
+            this.animator.SetBool("empty", false);
+        }
+        else if((this.remainingPoints < this.recoveryLife * 0.5f) && this.remainingPoints > 0)
+        {
+            this.animator.SetBool("full", false);
+            this.animator.SetBool("medium", false);
+            this.animator.SetBool("almostEmpty", true);
+            this.animator.SetBool("empty", false);
+        }
+        else
+        {
+            this.animator.SetBool("full", false);
+            this.animator.SetBool("medium", false);
+            this.animator.SetBool("almostEmpty", false);
+            this.animator.SetBool("empty", true);
+            
+        }
+
+        
+    }
+
+    IEnumerator DestroyPotion()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
     }
 }
